34a9b419e15c1f71d26146ad1dca83b2ba3c78d2
caiogatinho <ss.caioeduardo@gmail.com>
2023-01-20 21:25:35 -0300

    ok

--- circuits/Transducer/Transducer.ino
+++ circuits/Transducer/Transducer.ino
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream:circuits/Transducer/Transducer.ino
 int sensorPin = A0;
+=======
+int sensorPin = A3;
+>>>>>>> Stashed changes:codes/arduino/sensors/Transducer/Transducer.ino
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,11 +19,14 @@ void loop() {
   // transform analog read into temperature, using the cailbration table 
   // and function, got from comparing with a calibrated pump
   // data: [p(kgf/cm2), bits; 27, 289; 30, 306; 50, 386; 59, 415; 70, 464]
-  float pressure = 0.246*sensorValue - 45.208;  
-
+  float pressure = -73.9 + 0.423*sensorValue -2.75*pow(10,-4)*pow(sensorValue,2)+1.21*pow(10,-7)*pow(sensorValue,3);
   // print out the pressure:
   Serial.println(pressure);
 
   // delay in between reads for stability
+<<<<<<< Updated upstream:circuits/Transducer/Transducer.ino
   delay(1000);  
+=======
+  delay(500);  
+>>>>>>> Stashed changes:codes/arduino/sensors/Transducer/Transducer.ino
 }
