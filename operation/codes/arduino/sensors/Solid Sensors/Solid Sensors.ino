d23a38f44e894365ffa9635c375d5e7664a7913e
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2023-06-05 01:19:18 -0300

    Added second scale, prone to pin change

--- operation/codes/arduino/sensors/Solid Sensors/Solid Sensors.ino
+++ operation/codes/arduino/sensors/Solid Sensors/Solid Sensors.ino
@@ -1,27 +1,39 @@
 #include "HX711.h"
 
-#define DT A1
-#define SCK A0
+#define DT1 A1
+#define SCK1 A0
+#define DT2 A2
+#define SCK2 A4
 #define transducerPin A3
 
+#define SCALE1 436
+#define TARE1 50
+#define SCALE2 436
+#define TARE2 50
 
-HX711 escala; // Relaciona a variável escala
+#define BAUDRATE 115200
 
-void setup() {
- escala.begin (DT, SCK);
- Serial.begin(115200);
+HX711 escala1; // Relaciona a variável da 1a célula de carga
+HX711 escala2; // Relaciona a variável da 2a célula de carga
 
- escala.set_scale(436); // Utiliza uma escala padrão de verificação
- escala.tare(50); // Fixa o peso como tara
+void setup()
+{
+  Serial.begin(BAUDRATE);
+  escala1.begin(DT1, SCK1);
+  escala2.begin(DT2, SCK2);
+
+  escala1.set_scale(SCALE1); // Utiliza uma escala padrão de verificação
+  escala1.tare(TARE1);       // Fixa o peso como tara
+  escala2.set_scale(SCALE2);
+  escala2.tare(TARE2);
 }
 
-void loop() {
+void loop()
+{
   int pressureBits = analogRead(transducerPin);
-  float pressureBar = (0.309*pressureBits - 45.3)*1.03437368199072;
+  float pressureBar = (0.309 * pressureBits - 45.3) * 1.03437368199072;
 
-
-  Serial.println((String)millis() + "," + (String)escala.get_units() + "," + (String)pressureBar);
-
-
-//  delay(25);
+  // Sugestão de implementação da saída: TIMESTAMP, ESCALA1, ESCALA2, PRESSÃO
+  Serial.println(
+      (String)millis() + "," + (String)escala1.get_units() + "," + (String)escala2.get_units() + "," + (String)pressureBar);
 }
--- operation/codes/arduino/sensors/loadcell/loadcell.ino
+++ operation/codes/arduino/sensors/loadcell/loadcell.ino
@@ -1,25 +1,44 @@
 #include "HX711.h"
 
-#define DT A1
-#define SCK A0
+#define DT1 A1
+#define SCK1 A0
+#define DT2 A2
+#define SCK2 A4
+#define transducerPin A3
 
-HX711 escala; // Relaciona a variável escala
+#define SCALE1 436
+#define TARE1 50
+#define SCALE2 436
+#define TARE2 50
 
-void setup() {
- escala.begin (DT, SCK);
- Serial.begin(9600);
- Serial.print("Leitura da Tara: ");
- Serial.println(escala.read()); // Aguada o termino de verificação do peso
- Serial.println("Aguarde!");
- Serial.println("Iniciando ...");
- escala.set_scale(436); // Utiliza uma escala padrão de verificação
+#define BAUDRATE 9600
 
- escala.tare(20); // Fixa o peso como tara
- Serial.println("Insira o item para Pesar");
+HX711 escala1; // Relaciona a variável da 1a célula de carga
+HX711 escala2; // Relaciona a variável da 2a célula de carga
+
+void setup()
+{
+    Serial.begin(BAUDRATE);
+    escala1.begin(DT1, SCK1);
+    escala2.begin(DT2, SCK2);
+
+    Serial.print("Leitura da Tara 1: ");
+    Serial.println(escala1.read()); // Aguada o termino de verificação do peso
+    Serial.print("Leitura da Tara 2: ");
+    Serial.println(escala2.read()); // Aguada o termino de verificação do peso
+    Serial.println("Aguarde!");
+    Serial.println("Iniciando ...");
+    escala1.set_scale(SCALE1); // Utiliza uma escala padrão de verificação
+    escala2.set_scale(SCALE2); // Utiliza uma escala padrão de verificação
+
+    escala1.tare(TARE1); // Fixa o peso como tara
+    escala1.tare(TARE2); // Fixa o peso como tara
+    Serial.println("Insira os itens a pesar");
 }
 
-void loop() {
- Serial.print("Valor da Leitura: ");
- Serial.println(escala.get_units(10)); // Retorna peso descontada a tara
- delay(100);
+void loop()
+{
+    Serial.println("Leitura 1,\t\t Leitura 2: ");
+    Serial.println(escala1.get_units(10) + ",\t\t" + escala2.get_units(10)); // Retorna peso descontada a tara
+    delay(100);
 }
